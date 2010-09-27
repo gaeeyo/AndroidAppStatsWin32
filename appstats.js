ea5c6cf2963c145aa692e56170a0166440bad36f
gaeeyo <iknnofm0126@gmail.com>
2010-09-27 14:56:35 +0900

    tsvで書き出すように

--- appstats.js
+++ appstats.js
@@ -6,6 +6,7 @@
 var MAIN_URL = "http://market.android.com/publish/Home";
 var LOGIN_URL = "https://www.google.com/accounts/ServiceLogin";
 var INSERT_SCRIPT_PATH = 'browser_script.js';
+var TSV_DIR = './';
 
 main(WScript.Arguments);
 
@@ -49,6 +50,7 @@ function getStats(email, pass) {
 			if (ie.Document.Script.apps != undefined) {
 				WScript.Sleep(2000);
 				complete(ie.Document.Script.apps);
+				output_tsv(ie.Document.Script.apps, TSV_DIR);
 				//debug_ie(ie);
 				break;
 			}
@@ -68,6 +70,31 @@ function complete(apps) {
 		for (var key in app) {
 			log(key + " = " + app[key]);
 		}
+	}
+}
+
+function output_tsv(apps, dir) {
+	var fso = new ActiveXObject('Scripting.FileSystemObject');
+	var time = new Date();
+
+	for (var x in apps) {
+		var app = apps[x];
+		var f = fso.OpenTextFile(dir + app.packageName + ".tsv", 8, true);
+
+		var columns = [
+			time.toString(),
+			app.versionCode,
+			app.version,
+			app.total,
+			app.active,
+			app.stars[4],
+			app.stars[3],
+			app.stars[2],
+			app.stars[1],
+			app.stars[0]
+		];				
+		f.Write(columns.join("\t")+"\n");
+		f.close();
 	}
 }
 
